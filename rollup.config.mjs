f020a284018fa24a13e6f69f35329e2a152ea308
Fabio Cingottini <fabio.cingottini@gmail.com>
2024-10-23 11:22:06 +0200

    chore: add rollup.config.mjs file

---
+++ rollup.config.mjs
@@ -0,0 +1,33 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true,
+      exports: 'named',
+    },
+  ],
+  external: ['react', 'react-dom'],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    postcss({
+      modules: true,
+      use: ['sass'],
+      extract: true,
+    }),
+  ],
+};
