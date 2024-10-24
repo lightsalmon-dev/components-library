e02726e32f5f4ce8d8d96922e03ba9c7dc21743e
Fabio Cingottini <fabio.cingottini@gmail.com>
2024-10-24 18:01:17 +0200

    chore: avoid usage of import React statement

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -1,6 +1,6 @@
 import cn from "classnames";
-import React, { FC, PropsWithChildren } from "react";
+import type { FC, PropsWithChildren } from "react";
 
 export const Grid: FC<PropsWithChildren> = ({ children }) => {
-  return <div className={cn("ls-grid")}>{children}</div>;
+	return <div className={cn("ls-grid")}>{children}</div>;
 };
