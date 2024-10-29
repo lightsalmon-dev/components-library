0a92740c2fee5922ac460d59105e45538ebfa9f6
Fabio Cingottini <fabio.cingottini@gmail.com>
2024-10-29 17:01:51 +0100

    chore: fix base path in ladle config to correctly deploy to gh pages

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -6,4 +6,5 @@ export default {
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 	`,
 	outDir: "ladle-build",
+	base: "/components-library/",
 };
