a7804976c2e9ce8f7bc19b5dc569c77dcf70044b
Fabio Cingottini <fabio.cingottini@gmail.com>
2024-10-29 16:02:40 +0100

    chore(stories): add pipeline to deploy ladle storybook to github pages

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -5,4 +5,5 @@ export default {
   	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 	`,
+	outDir: "ladle-build",
 };
