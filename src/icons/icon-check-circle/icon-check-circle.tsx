d635d75a23a7e77370314cc9afec0c315301f34c
Fabio Cingottini <fabio.cingottini@gmail.com>
2024-10-30 16:55:50 +0100

    chore: introduce icons that will be used by input

---
+++ src/icons/icon-check-circle/icon-check-circle.tsx
@@ -0,0 +1,26 @@
+import cn from "classnames";
+import type { ComponentProps, FC } from "react";
+
+export const IconCheckCircle: FC<ComponentProps<"svg">> = ({
+	className,
+	...otherProps
+}) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth="1.5"
+			stroke="currentColor"
+			className={cn("ls-icon", className)}
+			{...otherProps}
+		>
+			<title>Check circle</title>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+			/>
+		</svg>
+	);
+};
