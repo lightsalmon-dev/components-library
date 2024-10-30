b7bb11c32abbb990d36bf1a7914a43228d3a4ac4
Fabio Cingottini <fabio.cingottini@gmail.com>
2024-10-30 16:57:50 +0100

    feat: introduce input component

---
+++ src/components/input/input.tsx
@@ -0,0 +1,114 @@
+import cn from "classnames";
+import {
+	type ComponentProps,
+	forwardRef,
+	useCallback,
+	useEffect,
+	useId,
+	useState,
+} from "react";
+import { IconCheckCircle } from "../../icons/icon-check-circle";
+import { IconExclamationCircle } from "../../icons/icon-exclamation-circle";
+
+type InputProps = ComponentProps<"input"> & {
+	label: string;
+	isValid: boolean;
+	isErrored: boolean;
+};
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+	(
+		{
+			className,
+			id,
+			label,
+			isValid,
+			isErrored,
+			onFocus,
+			onBlur,
+			...otherProps
+		},
+		ref,
+	) => {
+		const inputId = id ?? useId();
+		const statusId = useId();
+
+		const [isErrorTooltipOpen, setIsErrorTooltipOpen] = useState(false);
+		useEffect(() => {
+			if (isErrored) {
+				setIsErrorTooltipOpen(true);
+			}
+		}, [isErrored]);
+
+		const localOnFocus = useCallback<
+			Exclude<ComponentProps<"input">["onFocus"], undefined>
+		>(
+			(e) => {
+				setIsErrorTooltipOpen(false);
+				if (onFocus) {
+					onFocus(e);
+				}
+			},
+			[onFocus],
+		);
+
+		const localOnBlur = useCallback<
+			Exclude<ComponentProps<"input">["onBlur"], undefined>
+		>(
+			(e) => {
+				if (isErrored) {
+					setIsErrorTooltipOpen(true);
+				}
+				if (onBlur) {
+					onBlur(e);
+				}
+			},
+			[onBlur, isErrored],
+		);
+
+		return (
+			<div className={cn("ls-input-container", className)}>
+				<div className="ls-input-label-container">
+					<label htmlFor={inputId} className={cn("ls-input-label")}>
+						{label}
+					</label>
+					<span
+						id={statusId}
+						className="ls-error-tooltip-container"
+						aria-live="polite"
+					>
+						{isErrorTooltipOpen && (
+							<p className="ls-error-tooltip-text">
+								There is an error with the input.
+							</p>
+						)}
+					</span>
+					<IconCheckCircle
+						className={cn("ls-input-icon", "ls-icon-valid", {
+							"ls-icon-visible": isValid,
+						})}
+						aria-hidden={!isValid}
+					/>
+					<IconExclamationCircle
+						className={cn("ls-input-icon", "ls-icon-errored", {
+							"ls-icon-visible": isErrored,
+						})}
+						onClick={() => setIsErrorTooltipOpen(!isErrorTooltipOpen)}
+						aria-hidden={!isErrored}
+					/>
+				</div>
+				<input
+					id={inputId}
+					ref={ref}
+					className={cn("ls-input", {
+						"ls-valid": isValid,
+						"ls-errored": isErrored,
+					})}
+					aria-describedby={statusId}
+					onFocus={localOnFocus}
+					onBlur={localOnBlur}
+					{...otherProps}
+				/>
+			</div>
+		);
+	},
+);
