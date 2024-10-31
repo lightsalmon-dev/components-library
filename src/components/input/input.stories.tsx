4c0f229f4a8d8388e9ae46e09664118bf1f936c1
Fabio Cingottini <fabio.cingottini@gmail.com>
2024-10-31 11:25:07 +0100

    chore(stories): adjust broken Input story

--- src/components/input/input.stories.tsx
+++ src/components/input/input.stories.tsx
@@ -10,6 +10,7 @@ export const All: Story = () => {
 				label="Unfilled"
 				isValid={false}
 				isErrored={false}
+				errorMessage="Change me via errorMessage prop"
 			/>
 			<Input
 				placeholder="Placeholder"
@@ -17,20 +18,23 @@ export const All: Story = () => {
 				defaultValue={"This the value"}
 				isValid={false}
 				isErrored={false}
+				errorMessage="Change me via errorMessage prop"
 			/>
 			<Input
 				placeholder="Placeholder"
 				label="Valid"
-				defaultValue={"This is a valid value"}
+				defaultValue="I'm valid because isValid is true"
 				isValid={true}
 				isErrored={false}
+				errorMessage="Change me via errorMessage prop"
 			/>
 			<Input
 				placeholder="Placeholder"
 				label="Errored"
-				defaultValue={"This is an errored value"}
+				defaultValue="I'm errored because isErrored is true"
 				isValid={false}
 				isErrored={true}
+				errorMessage="Change me via errorMessage prop"
 			/>
 		</div>
 	);
